ec4fcb0141a236f617c85f9337658b16f625f75b
tichen4869 <50885738+tichen4869@users.noreply.github.com>
2019-06-10 22:28:10 -0400

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This notebook will mainly be used for the Course Capstone project
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
